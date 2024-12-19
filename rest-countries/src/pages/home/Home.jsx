d42f44ceaf6c8d9e24d4649f860966115b067328
Anderson Henrique <dev24@redepos.com.br>
2024-12-19 12:15:40 -0300

    titles

--- rest-countries/src/pages/home/Home.jsx
+++ rest-countries/src/pages/home/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   return (
     <>
-        <h1>Home</h1>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          borderBottom: "1px solid gray",
+          padding: "20px 16px",
+        }}
+      >
+        <Typography variant="h3" sx={{fontSize: '1rem'}}>
+          Where in the world?
+        </Typography>
+
+        <Typography variant="h3" sx={{fontSize: '1rem'}}>
+          Dark Mode
+        </Typography>
+      </Box>
+
+        {/* Box do inputs */}
+      <Box>
+
+      </Box>
     </>
   );
 };
