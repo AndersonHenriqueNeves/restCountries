9015b62555b4c53158d13b9f4676def52ceb0cf8
Werica <wericabarbosa22@gmail.com>
2024-12-23 14:54:02 +0100

    Começando projeto

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,33 +1,82 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import SearchIcon from '@mui/icons-material/Search';
+import { styled } from '@mui/system';
+
+
+const StyledLabel = styled('label')({
+  display: 'flex',
+  alignItems: 'center',
+  width: '100%',
+  color: '#2B3743',
+  padding: '8px 16px',
+  borderRadius: '4px',
+  backgroundColor: '#f5f5f5', 
+  cursor: 'pointer',
+});
 
 const Home = () => {
   return (
     <>
       <Box
         sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "row",
-          borderBottom: "1px solid gray",
-          padding: "20px 16px",
+          background: "#202D36",
+          marginBottom: "20px",
         }}
       >
-        <Typography variant="h3" sx={{fontSize: '1rem'}}>
-          Where in the world?
-        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "row",
+            borderBottom: "1px solid gray",
+         
+          }}
+        >
+          <Typography variant="h3" sx={{ fontSize: '1rem', color: "white" }}>
+            Where in the world?
+          </Typography>
 
-        <Typography variant="h3" sx={{fontSize: '1rem'}}>
-          Dark Mode
-        </Typography>
+          <Typography variant="h3" sx={{ fontSize: '1rem', color: "white" }}>
+            Dark Mode
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            padding: '20px', 
+            background:"#2B3743"
+          }}
+        >
+          <StyledLabel>
+            <SearchIcon sx={{ marginRight: '8px', color: 'white' }} />
+            <Typography variant="body1" sx={{ color: '#000' }}>
+              Buscar
+            </Typography>
+          </StyledLabel>
+        </Box>
       </Box>
 
-        {/* Box do inputs */}
-      <Box>
 
-      </Box>
+{/* 
+      <header></header>
+      <search></search>
+      <section>
+          <select name="" id=""></select>
+      </section>
+      <section>
+        for
+        <div>
+          <img src="" alt="" />
+          <h3></h3>
+
+          Typography  
+          Typography  
+          Typography  
+        </div>
+      </section> */}
     </>
   );
 };
